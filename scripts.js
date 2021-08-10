13660f86b86f75739e63a85805e2f8ea93b8f86f
Laura DiSanza <ldisanza7@gmail.com>
2021-08-10 13:11:52 -0400

    Then and now seconds

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//setInterval() does not work very well if you are away from your browser or on IOS
+
+function timer(seconds) {
+    const now = Date.now(); //Give us the current time stamp
+    const then = now + seconds * 1000;
+}
